fbd1b0b9ed4db4817b78b78423ff2e35b5e1eeb1
Felipe Alfonso González <f.alfonso.go@gmail.com>
2022-02-24 04:27:56 -0300

    js delete twitts

---
+++ js-twitts-delete.js
@@ -0,0 +1,22 @@
+
+/*
+INSERT IN THE CONSOLE OF CHROME
+
+
+
+*/
+
+
+setInterval(
+            function() {
+            t = $( '.js-actionDelete button' ); // get delete buttons
+            for ( i = 0; true; i++ ) { // count removed
+            if ( i >= t.length ) { // if removed all currently available
+            window.scrollTo( 0, $( document ).height() ); // scroll to bottom of page - loads more
+            return
+            }
+            $( t[i] ).trigger( 'click' ); // click and remove button from dom
+            $( 'button.delete-action' ).trigger( 'click' ); // click and remove button from dom
+            }
+            }, 2000
+            )
